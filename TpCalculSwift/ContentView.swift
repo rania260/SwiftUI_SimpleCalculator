d9dbf564b2dc2968f7155fab7946c0b64f900460
rania260 <raniarezgui4@gmail.com>
2024-04-21 21:18:44 +0100

    first commit

--- TpCalculSwift/ContentView.swift
+++ TpCalculSwift/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberone = ""
+    @State private var number = ""
+    @State private var result = "0"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20){
+            Image("cal")
+                    .resizable()
+                    .frame(width: 200, height:100)
+            TextField("Num 1", text: $numberone)
+            TextField("Num 2", text: $number)
+            
+            HStack {
+                Button("+") {
+                    if let val1 = Double (self.numberone),
+                       let val2 = Double (self.number){
+                        self.result = String( val1 + val2)
+                    }
+            }
+            Button("-") {
+                if let val1 = Double (self.numberone),
+                   let val2 = Double (self.number){
+                    self.result = String( val1 - val2)
+                }
+            }
+            Button("*") {
+                if let val1 = Double (self.numberone),
+                   let val2 = Double (self.number){
+                    self.result = String( val1 * val2)
+                }
+            }
+            Button("/") {
+                if let val1 = Double (self.numberone),
+                   let val2 = Double (self.number){
+                    self.result = String( val1 / val2)
+                }
+            }
+            }
+            
+            Text("Result: \(result)")
+                .font(.title)
+            
+        
+        }
+        .padding()
+            
     }
 }
 
